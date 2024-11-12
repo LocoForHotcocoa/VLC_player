18a1c28c8cfb8ef237f4c6a71468199eae01b560
Matthew Bradley <60621409+LocoForHotcocoa@users.noreply.github.com>
2024-11-12 14:50:14 -0800

    Update vlc_controller.py

--- tools/vlc_controller.py
+++ tools/vlc_controller.py
@@ -4,9 +4,10 @@ vlc_process = None
 
 def run_vlc(playlist_file: str) -> None:
 	global vlc_process
-	# vlc playlist.m3u --extraintf http --http-port 8080 --http-password admin
+	
 	# runs this command in new subprocess, with no output or error messages (I think my VLC is a little buggy)
-	vlc_process = subprocess.Popen(['vlc', playlist_file, '--extraintf', 'http', '--http-port', '8080', '--http-password', 'admin'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	cmd = f'vlc {playlist_file} --extraintf http --http-port 8080 --http-password admin'
+	vlc_process = subprocess.Popen([cmd], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 	vlc_process.wait()
 
