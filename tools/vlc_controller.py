671af471c10e271594c974b6420591774069a31b
Matthew Bradley <60621409+LocoForHotcocoa@users.noreply.github.com>
2024-11-12 22:58:42 -0800

    fixed vlc player subprocess

--- tools/vlc_controller.py
+++ tools/vlc_controller.py
@@ -7,7 +7,7 @@ def run_vlc(playlist_file: str) -> None:
 	
 	# runs this command in new subprocess, with no output or error messages (I think my VLC is a little buggy)
 	cmd = f'vlc {playlist_file} --extraintf http --http-port 8080 --http-password admin'
-	vlc_process = subprocess.Popen([cmd], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	vlc_process = subprocess.Popen([cmd], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 
 	vlc_process.wait()
 
